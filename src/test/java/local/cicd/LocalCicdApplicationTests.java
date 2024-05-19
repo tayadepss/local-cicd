4c983d06c3fecd3bab1b66690068c31ab46947b6
tayadepss <tayadepss@gmail.com>
2024-05-19 16:12:42 +0530

    fist commit

---
+++ src/test/java/local/cicd/LocalCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package local.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
